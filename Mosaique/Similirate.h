165bafa545c67acea60d3f85238751d0755d7d3f
Margot <gotgot07@hotmail.fr>
2020-02-04 14:38:49 +0100

    fonction similarité somme et moyenne

--- Mosaique/Similirate.h
+++ Mosaique/Similirate.h
@@ -31,16 +31,56 @@ int diffVal(Image im, Image imCompare)
 	{
 		for (int y = 0; y < heightMax; y++)
 		{
-			diff[0] = im(x, y).x - imCompare(x, y).x;
-			diff[1] = im(x, y).y - imCompare(x, y).y;
-			diff[2] = im(x, y).z - imCompare(x, y).z;
-			somme = diff[0] + diff[1] + diff[2];
+			diff[0] = abs(im(x, y).x - imCompare(x, y).x);
+			diff[1] = abs(im(x, y).y - imCompare(x, y).y);
+			diff[2] = abs(im(x, y).z - imCompare(x, y).z);
+			somme += diff[0] + diff[1] + diff[2];
 		}
 	}
 
-	std::cerr << "diffVal() : " << diff[0] << ", " << diff[1] << ", " << diff[2] << std::endl;
-	std::cerr << "diffVal() : " << somme;
+	//std::cerr << "diffVal() : " << somme;
 	return somme;
+
+}
+
+int diffValMoyenne(Image im, Image imCompare)
+{
+	int moy = 0;
+	int diff[] = { 0, 0, 0 };
+	int heightMax = 0;
+	int widthMax = 0;
+
+	if (im.getHeight() > imCompare.getHeight())
+	{
+		heightMax = im.getHeight();
+	}
+	else
+	{
+		heightMax = imCompare.getHeight();
+	}
+
+	if (im.getWidth() > imCompare.getWidth())
+	{
+		widthMax = im.getWidth();
+	}
+	else
+	{
+		widthMax = imCompare.getWidth();
+	}
+
+	for (int x = 0; x < widthMax; x++)
+	{
+		for (int y = 0; y < heightMax; y++)
+		{
+			diff[0] = abs(im(x, y).x - imCompare(x, y).x);
+			diff[1] = abs(im(x, y).y - imCompare(x, y).y);
+			diff[2] = abs(im(x, y).z - imCompare(x, y).z);
+			moy += ((diff[0] + diff[1] + diff[2]) / 3);
+		}
+	}
+
+	std::cerr << "diffValMoyenne() : " << moy;
+	return moy;
 
 }
 
