bdb8e0547af3bc6ad6ee5a09ff96bcbf81d2b4e7
Attika <attika.mayou@gmail.com>
2020-02-06 11:16:56 +0100

    Ajout diffHistoBin()

--- Mosaique/Mosaique.cpp
+++ Mosaique/Mosaique.cpp
@@ -15,19 +15,19 @@ int main()
 	string imagePath;
 	cout << u8"Chemin de l'image à modifer ?" << "\n";
 	cin >> imagePath;
-	/*
+	
 	string imageBisPath;
 	cout << u8"Chemin de l'image à comparer ?" << "\n";
 	cin >> imageBisPath;
-	*/
+	
 	cout << u8"Chargement des images" << "\r";
 	Image baseImage(imagePath.c_str());
-	//Image compareImage(imageBisPath.c_str());
+	Image compareImage(imageBisPath.c_str());
 	cout << u8"Images chargées avec succès !" << "\n";
 
 	baseImage.convert(HSV);
-	//compareImage.convert(HSV);
-	cut(baseImage, 10, 10);
+	compareImage.convert(HSV);
+	//cut(baseImage, 10, 10);
 
 	string databasePath;
 	cout << u8"Chemin du dossier contenant la base de données d'image ?" << "\n";
@@ -38,15 +38,18 @@ int main()
 	cout << u8"Base de données chargée avec succès !" << "\n";
 
 	baseImage.convert(RGB);
-	//compareImage.convert(RGB);
+	compareImage.convert(RGB);
 	
+	diffHisto(baseImage, compareImage);
+	diffHistoBin(baseImage, compareImage, 16);
+
 	/*imCompare.convert(HSV);
 	diffVal(im, imCompare);
 	cropTopLeft(imCompare);
 	imCompare.convert(RGB);*/
 
 	baseImage.save("save.jpg", baseImage.getBaseFormat());
-	//compareImage.save("saveBis.jpg", compareImage.getBaseFormat());
+	compareImage.save("saveBis.jpg", compareImage.getBaseFormat());
 	return 0;
 }
 
